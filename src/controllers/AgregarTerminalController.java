d65c9194606e976476506763eb56acaec905f413
pedrox10 <barcocarrasco.pedro@gmail.com>
2025-03-12 18:00:27 -0400

    Primer commit 12 Mar

---
+++ src/controllers/AgregarTerminalController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AgregarTerminalController implements Initializable {
+
+    @Override
+    public void initialize(URL event, ResourceBundle rb) {
+
+    }
+}
---
+++ src/models/Respaldo.java
@@ -0,0 +1,15 @@
+package models;
+
+import org.orman.mapper.Model;
+import org.orman.mapper.annotation.Entity;
+import org.orman.mapper.annotation.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class Respaldo extends Model<Respaldo> {
+    @PrimaryKey(autoIncrement = true)
+    public int id;
+    public Date fecha;
+    public Terminal terminal;
+}
