1c2d4e951117b6ba0331578df19c76b0549944b4
pedrox10 <barcocarrasco.pedro@gmail.com>
2025-03-26 18:05:39 -0400

    desde work 26 Marzov2

--- src/controllers/ListarRespaldosController.java
+++ src/controllers/ListarRespaldosController.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import models.Respaldo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +20,8 @@ public class ListarRespaldosController implements Initializable {
     public TableColumn tc_fecha;
     public TableColumn tc_nombre;
     public TableColumn tc_sincronizado;
+   // ObservableList<Respaldo> respaldos = FXCollections.observableArrayList();
+    //ObjectProperty<Respaldo> op_seleccionado = new SimpleObjectProperty();
 
     @Override
     public void initialize(URL event, ResourceBundle rb) {
